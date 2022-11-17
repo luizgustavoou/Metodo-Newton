6f1133332f16d9858a58baab9636bf3a737985ff
luizgustavoou <89609312+luizgustavoou@users.noreply.github.com>
2022-11-17 09:43:10 -0300

    Feature: montar esqueleto do programa

---
+++ newton.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+//
+//
+//
+
+int main() {
+    int valor_anterior;
+    int valor_inicial=2;
+    double limite = pow(10, -4);
+    double erro;
+
+    while (1) {
+        valor_anterior = valor_inicial;
+        //valor_inicial = valor_inicial - f(x) / f'(x);
+
+        /* erro = abs(valor_inicial-valor_anterior) / valor_inicial;
+        if(erro < limite) break; */
+    }
+
+    return 0;
+}
