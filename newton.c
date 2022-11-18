8a29e2cb69036fbccb92826856271bbecf52fa9f
luizgustavoou <89609312+luizgustavoou@users.noreply.github.com>
2022-11-17 21:40:53 -0300

    feature: Adicionar exemplo de funcao

--- newton.c
+++ newton.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <math.h>
 
-//
-//
-//
+double f(double x) {
+    return x*x-x-2.0;
+}
+
+double df(double x) {
+    //Outra forma por meio da aproximação de h muito próximo de 0 :
+    const double h = 0.0001;
+    return ((f(x+h)-f(x))/h);
+
+    //return 2*x-1;
+}
+
 
 int main() {
     int valor_anterior;
-    int valor_inicial=2;
+    double valor_inicial=1.0;
     double limite = pow(10, -4);
     double erro;
 
     while (1) {
         valor_anterior = valor_inicial;
-        //valor_inicial = valor_inicial - f(x) / f'(x);
+        valor_inicial = valor_anterior - (f(valor_anterior) / df(valor_anterior));
 
-        /* erro = abs(valor_inicial-valor_anterior) / valor_inicial;
-        if(erro < limite) break; */
+        erro = abs(valor_inicial-valor_anterior) / valor_inicial;
+        if(erro < limite) break;
     }
+
+    printf("Valor: %lf\n", valor_inicial);
 
     return 0;
 }
