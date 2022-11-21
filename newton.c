2d3b74a3c91a54334d04368b748d7af65b7e5ad9
luizgustavoou <luizgustavooumbelino@gmail.com>
2022-11-21 13:24:59 -0300

    feature: adicionar funcao fb e sua derivada

--- newton.c
+++ newton.c
@@ -2,18 +2,15 @@
 #include <math.h>
 
 #define MAX 101
-double f(double x) {
+double fa(double x) {
     return 0.9-(1+x+(x*x)/2)*exp(-x);
 }
 
-double df(double x) {
-    //Outra forma por meio da aproximação de h muito próximo de 0 :
-    // return ((f(x+h)-f(x))/h);
-
+double dfa(double x) {
     return exp(-x)*(x*x)/2;
 }
 
-int main() {
+void rootFA() {
     double valor_anterior = 0.0, valor_inicial= 1.1;
 
     double limite = pow(10, -6);
@@ -24,14 +21,48 @@ int main() {
         cont++;
         if (cont >= MAX) break;
         valor_anterior = valor_inicial;
-        valor_inicial = valor_anterior - (f(valor_anterior) / df(valor_anterior));
+        valor_inicial = valor_anterior - (fa(valor_anterior) / dfa(valor_anterior));
 
         erro = abs(valor_inicial-valor_anterior) / abs(valor_inicial);
-        printf("%lf\n", erro);
+        //printf("%lf\n", erro);
         if(erro < limite) break;
     }
 
-    printf("Raiz %lf -> F(%lf) = %lf\n", valor_inicial, valor_inicial, f(valor_inicial));
+    printf("Raiz de fa %lf -> Fa(%lf) = %lf\n", valor_inicial, valor_inicial, fa(valor_inicial));
+}
 
+double fb(double x) {
+    return 0.1-(1+x+(x*x)/2)*exp(-x);
+}
+
+double dfb(double x) {
+    return exp(-x)*(x*x)/2;
+}
+
+void rootFB() {
+    double valor_anterior = 0.0, valor_inicial= 5.1;
+
+    double limite = pow(10, -6);
+    double erro;
+
+    int cont=0;
+    while (1) {
+        cont++;
+        if (cont >= MAX) break;
+        valor_anterior = valor_inicial;
+        valor_inicial = valor_anterior - (fb(valor_anterior) / dfb(valor_anterior));
+
+        erro = abs(valor_inicial-valor_anterior) / abs(valor_inicial);
+        //printf("%lf\n", erro);
+        if(erro < limite) break;
+    }
+
+    printf("Raiz de fb %lf -> Fb(%lf) = %lf\n", valor_inicial, valor_inicial, fb(valor_inicial));
+}
+
+int main() {
+
+    rootFA();
+    rootFB();
     return 0;
 }
