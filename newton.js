fce015a38b5b5babf55e0ebb13c19dcc3dbd197e
luizgustavoou <89609312+luizgustavoou@users.noreply.github.com>
2022-11-21 15:26:10 -0300

    Update newton.js

--- newton.js
+++ newton.js
@@ -1,6 +1,6 @@
 const MAX = 101;
-const fa_str = "0.9-(1+x+x²/2)*e^(-x)";
-const fb_str = "0.1-(1+x+x²/2)*e^(-x)";
+const fa_str = "0.9 - (1+x+x²/2)*e^(-x)";
+const fb_str = "0.1 - (1+x+x²/2)*e^(-x)";
 const { abs, e, pow, round } = require("mathjs");
 
 function fa(x) {
@@ -37,6 +37,7 @@ function rootFunction(start_value, f, df, func) {
     if (error < limit) break;
   }
 
+  return xn;
   console.log(`Raiz da função ${func} -> F(${xn}) = ${round(f(xn))}`);
 }
 
@@ -50,8 +51,7 @@ var input = readline.createInterface({
 const q1 = () => {
   return new Promise((res, rej) => {
     input.question(`Chute inicial para a função ${fa_str}: `, (answer) => {
-      rootFunction(parseFloat(answer), fa, dfa, fa_str);
-      res();
+      res(rootFunction(parseFloat(answer), fa, dfa, fa_str));
     });
   });
 };
@@ -59,15 +59,17 @@ const q1 = () => {
 const q2 = () => {
   return new Promise((res, rej) => {
     input.question(`Chute inicial para a função ${fb_str}: `, (answer) => {
-      rootFunction(parseFloat(answer), fb, dfb, fb_str);
-      res();
+      res(rootFunction(parseFloat(answer), fb, dfb, fb_str));
     });
   });
 };
 
 const main = async () => {
-  await q1();
-  await q2();
+  const xa = await q1();
+  const xb = await q2();
+  console.log(`Raiz da função ${fa_str} -> F(${xa}) = ${round(fa(xa))}`);
+  console.log(`Raiz da função ${fb_str} -> F(${xb}) = ${round(fb(xb))}`);
+  console.log(`O tempo de subida é ${xb-xa}`);
   input.close();
 };
 
