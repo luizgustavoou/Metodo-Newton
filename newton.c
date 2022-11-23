cea6e054c196fe0805bf86ba8942de89584081a1
luizgustavoou <89609312+luizgustavoou@users.noreply.github.com>
2022-11-23 12:47:18 -0300

    feature: refatorar codigo (by Jady)

--- newton.c
+++ newton.c
@@ -1,68 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 #define MAX 101
-double fa(double x) {
-    return 0.9-(1+x+(x*x)/2)*exp(-x);
+
+double funcao(double x, double valor_g)
+{
+    return valor_g - (1 + x + pow(x, 2)/2) * exp(-x);
 }
 
-double dfa(double x) {
-    return exp(-x)*(x*x)/2;
+double derivada_funcao(double x)
+{
+    return exp(-x) * pow(x, 2)/2;
 }
 
-void rootFA() {
-    double valor_anterior = 0.0, valor_inicial= 1.1;
+double raiz(double valor_inicial, double valor_g)
+{
+    double valor_anterior = 0.0;
 
     double limite = pow(10, -6);
     double erro;
 
-    int cont=0;
+    int cont = 0;
     while (1) {
         cont++;
         if (cont >= MAX) break;
         valor_anterior = valor_inicial;
-        valor_inicial = valor_anterior - (fa(valor_anterior) / dfa(valor_anterior));
+        valor_inicial = valor_anterior - (funcao(valor_anterior, valor_g) / derivada_funcao(valor_anterior));
 
         erro = abs(valor_inicial-valor_anterior) / abs(valor_inicial);
-        //printf("%lf\n", erro);
         if(erro < limite) break;
     }
 
-    printf("Raiz de fa %lf -> Fa(%lf) = %lf\n", valor_inicial, valor_inicial, fa(valor_inicial));
+    printf("Raiz de f %lf -> F(%lf) = %lf\n", valor_inicial, valor_inicial, funcao(valor_inicial, valor_g));
+
+  return valor_inicial; //funcao(valor_inicial, valor_g)
 }
 
-double fb(double x) {
-    return 0.1-(1+x+(x*x)/2)*exp(-x);
-}
 
-double dfb(double x) {
-    return exp(-x)*(x*x)/2;
-}
+int main()
+{
+    double raiz_A = raiz(1.1, 0.9);
+    double raiz_B = raiz(5.1, 0.1);
 
-void rootFB() {
-    double valor_anterior = 0.0, valor_inicial= 5.1;
+    printf("O tempo de subida é: %lf \n", (raiz_B - raiz_A));
 
-    double limite = pow(10, -6);
-    double erro;
-
-    int cont=0;
-    while (1) {
-        cont++;
-        if (cont >= MAX) break;
-        valor_anterior = valor_inicial;
-        valor_inicial = valor_anterior - (fb(valor_anterior) / dfb(valor_anterior));
-
-        erro = abs(valor_inicial-valor_anterior) / abs(valor_inicial);
-        //printf("%lf\n", erro);
-        if(erro < limite) break;
-    }
-
-    printf("Raiz de fb %lf -> Fb(%lf) = %lf\n", valor_inicial, valor_inicial, fb(valor_inicial));
-}
-
-int main() {
-
-    rootFA();
-    rootFB();
     return 0;
 }
